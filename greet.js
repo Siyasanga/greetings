e76b5ecda401d230d683defd74287e64906228f4
Siyasanga <siyasanga@live.co.za>
2017-03-01 16:21:44 +0200

    first commit

---
+++ greet.js
@@ -0,0 +1,34 @@
+var text = document.querySelector('.text');
+var greeting = document.querySelector(".greeting");
+var sub = document.querySelector(".button");
+var namesGreeted = {};
+var counter = document.querySelector(".counter");
+if(typeof(localStorage.count)==undefined){
+  localStorage.setItem("count",0);
+}
+counter.innerHTML = localStorage.count;
+sub.addEventListener('click', function() {
+  if(namesGreeted[text.value] !== undefined){
+    //update the DOM to display the counter
+    return;
+  }
+  namesGreeted[text.value] = 1;
+  localStorage.count++;
+  counter.innerHTML = localStorage.count;
+  var radios = document.getElementsByName("lang");
+  for(var i=0; i < 3; i++){
+    if(radios[i].checked){
+      switch (i) {
+        case 0:
+          greeting.innerHTML = "Molo, "+text.value+"!";
+          break;
+        case 1:
+          greeting.innerHTML = "Hello, "+text.value+"!";
+          break;
+        case 2:
+          greeting.innerHTML = "Dumela, "+text.value+"!";
+      } // switch
+      break;
+    } // end of if
+  } // end of for
+});// End event listener
